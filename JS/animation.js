3b49faf435d98390a8d489c6c3d83cb38ee940c7
Yury <kiryaev.yury@yandex.ru>
2020-05-11 02:18:29 +0300

    +animation

---
+++ JS/animation.js
@@ -0,0 +1,5 @@
+const element = document.querySelector('.my-element');
+element.style.setProperty('--animate__fadeInUp', '0.9s');
+
+const element = document.querySelector('.my-element');
+element.classList.add('animate__animated', 'animate__fadeInUp');
